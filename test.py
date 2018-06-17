a36555285c13112c4e16616783840e60694afd9c
Eli Heuer <eliheuer@gmail.com>
2018-06-17 11:58:24 -0400

    Hello world

---
+++ test.py
@@ -0,0 +1,10 @@
+
+print('hello world'*10)
+print(231231 * 646464)
+fill(0.95, 0.95, 0.92)
+stroke(0)
+strokeWidth(0.5)
+rect(254, 72, 500 ,842)
+
+rect(200, 200, 340, 50)
+saveImage('MyExamplePage.pdf')
